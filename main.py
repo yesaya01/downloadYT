6d0a10782597211939f5545532106aff6fd22597
yesaya01 <129833319+yesaya01@users.noreply.github.com>
2023-04-04 07:59:32 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_templates
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=80)
